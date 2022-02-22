d6769406f83eced649b57ef71d4ec71da75ea6bb
ShinDongHwi <ehdgnl8940@naver.com>
2022-02-22 23:50:01 +0900

    Project Setting - Version Manage

---
+++ buildSrc/src/main/java/base/BuildBaseConfig.kt
@@ -0,0 +1,10 @@
+package base
+
+object BuildBaseConfig{
+    const val compileVersion = 31
+    const val buildToolsVersion = "30.0.2"
+    const val minSdkVersion = 26
+    const val targetSdkVersion = 31
+    const val composeVersion = "1.1.0"
+    const val kotlinVersion = "1.5.31"
+}
