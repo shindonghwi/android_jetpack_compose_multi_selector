ed778d9b65b483283c24b7f3b022fef22d780f75
ShinDongHwi <ehdgnl8940@naver.com>
2022-02-23 00:27:11 +0900

    feat: theme setting

--- app/src/main/java/com/example/medium_jetpackcompose/ui/theme/Theme.kt
+++ app/src/main/java/com/example/medium_jetpackcompose/ui/theme/Theme.kt
@@ -6,33 +6,24 @@ import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 
-private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
+private val LightThemeColors = lightColors(
+    primary = Primary500,
+    secondary = Gray900,
+    error = Red900
 )
 
-private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
-
-    /* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
-    onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
-    */
+private val DarkThemeColors = darkColors(
+    primary = Primary500,
+    secondary = Gray900,
+    error = Red900
 )
 
 @Composable
 fun Medium_JetpackComposeTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable() () -> Unit) {
     val colors = if (darkTheme) {
-        DarkColorPalette
+        DarkThemeColors
     } else {
-        LightColorPalette
+        LightThemeColors
     }
 
     MaterialTheme(
