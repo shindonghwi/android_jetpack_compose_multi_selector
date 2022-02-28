81b51e144837139ba26b116a1ecf2f35479d529b
ShinDongHwi <ehdgnl8940@naver.com>
2022-02-28 22:41:11 +0900

    chore: compose version upgrade

--- buildSrc/src/main/java/base/BuildBaseConfig.kt
+++ buildSrc/src/main/java/base/BuildBaseConfig.kt
@@ -7,10 +7,10 @@ object BuildBaseConfig{
     const val targetSdkVersion = 31
     const val kotlinVersion = "1.5.31"
 
-    const val composeVersion = "1.0.5"
-    const val composeConstraint = "1.0.0-rc01"
+    const val composeVersion = "1.1.1"
+    const val composeConstraint = "1.0.0"
     const val composeCoil = "1.4.0"
-    const val composeViewPager = "0.12.0"
+    const val composeViewPager = "0.19.0"
     const val composeViewBinding = "1.0.5"
     const val composeActivity = "1.3.0"
     const val composeSwipeRefresh = "0.23.0"
