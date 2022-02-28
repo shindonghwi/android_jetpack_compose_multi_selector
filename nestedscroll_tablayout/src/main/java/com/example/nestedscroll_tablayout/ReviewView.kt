58e76127e9ff5d4e08407754e1401d5d9bd645fa
ShinDongHwi <ehdgnl8940@naver.com>
2022-02-28 23:20:16 +0900

    feat: sample menu 4개 추가

---
+++ nestedscroll_tablayout/src/main/java/com/example/nestedscroll_tablayout/ReviewView.kt
@@ -0,0 +1,29 @@
+package com.example.nestedscroll_tablayout
+
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.resource.Primary500
+
+@Composable
+fun ReviewView() {
+
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentHeight()
+            .padding(horizontal = 20.dp)
+    ) {
+
+        repeat(50) {
+            Text("리뷰 $it", color = Primary500, style = MaterialTheme.typography.subtitle2)
+        }
+    }
+}
